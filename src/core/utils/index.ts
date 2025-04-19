9897e321d8f359a2654ade24e3f1f61fd991e38d
juratbek <juratbekxudayberganov9@gmail.com>
2025-04-19 18:52:31 +0500

    changes

--- src/core/utils/index.ts
+++ src/core/utils/index.ts
@@ -1,3 +1,28 @@
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
 export const eskizSMS = (code: number) => {
   return `ClinicsPlus platformasiga ro‘yxatdan o‘tish uchun tasdiqlash kodi: ${code}`;
+};
+
+export const updateEnvFile = (key: string, newValue: string) => {
+  try {
+    const envFile = fs.readFileSync('.env', 'utf8');
+    const lines = envFile.split('\n');
+
+    const updatedLines = lines.map((line) =>
+      line.startsWith(`${key}=`) ? `${key}=${newValue}` : line,
+    );
+
+    fs.writeFileSync('.env', updatedLines.join('\n'));
+    dotenv.config({ override: true });
+  } catch (error) {
+    console.error('Error updating env file:', error);
+  }
+};
+
+export const getEnvValue = (key: string): string | undefined => {
+  const env = fs.readFileSync('.env', 'utf8');
+  const line = env.split('\n').find((line) => line.startsWith(`${key}=`));
+  return line?.split('=')[1];
 };
--- src/modules/sms/sms.service.ts
+++ src/modules/sms/sms.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
-import { eskizSMS } from '../../core/utils';
+import { eskizSMS, getEnvValue, updateEnvFile } from '../../core/utils';
+import * as dotenv from 'dotenv';
 import { SendSmsDto } from './dtos';
 
 @Injectable()
@@ -19,21 +20,61 @@ export class SmsService {
         from: 4546,
       };
 
-      const token = process.env.ESKIZ_TOKEN;
+      const api = await this.getApi();
+      await api.post('/message/sms/send', axiosFormData);
 
-      const res = await axios.post(
-        'https://notify.eskiz.uz/api/message/sms/send',
-        axiosFormData,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      );
-
-      console.log(res.data);
+      return {
+        message: 'SUCCESS',
+      };
     } catch (error) {
       console.error(error);
     }
+  }
+
+  private async generateToken() {
+    try {
+      const api = await this.getApi();
+      const res = await api.post(`/auth/login`, {
+        email: process.env.ESKIZ_LOGIN,
+        password: process.env.ESKIZ_PASSWORD,
+      });
+
+      return res?.data?.data?.token as string;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async getApi() {
+    const api = axios.create({
+      baseURL: `https://notify.eskiz.uz/api`,
+    });
+
+    api.interceptors.request.use(async (config) => {
+      const token = getEnvValue('ESKIZ_TOKEN');
+
+      config.headers.Authorization = `Bearer ${token}`;
+      return config;
+    });
+
+    api.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        if (error.response?.status === 401) {
+          const newToken = await this.generateToken();
+
+          updateEnvFile('ESKIZ_TOKEN', newToken);
+
+          dotenv.config({ override: true });
+
+          error.config.headers.Authorization = `Bearer ${newToken}`;
+          return axios(error.config);
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return api;
   }
 }
